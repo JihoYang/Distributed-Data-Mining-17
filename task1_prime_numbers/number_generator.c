3e4da6aa51b73336508a2eb78e0a4030da5e59d5
Jiho Yang <jy1737@my.bristol.ac.uk>
2017-05-29 10:41:38 +0200

    added number generator

---
+++ task1_prime_numbers/number_generator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>                                                                       
+#include <stdlib.h>
+
+int main(){
+    unsigned long MAX_NUM = 10e7;
+    FILE *fp=NULL;
+    fp = fopen("input_numbers.txt", "w");
+    for (unsigned long i = 0; i < MAX_NUM; i++){
+        fprintf(fp, "%lu ", i+1);
+        printf("index = %lu\n", i);
+    }
+    return 0;
+}
