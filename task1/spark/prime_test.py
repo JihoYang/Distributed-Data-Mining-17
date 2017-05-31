2886c19a380de2952f4c49fee6464c1d8e192ffc
root <root@vm-10-155-208-238.cloud.mwn.de>
2017-05-31 02:32:37 +0200

    removed previous files. added task1 directory and spark directory along with prime_py codes

---
+++ task1/spark/prime_test.py
@@ -0,0 +1,66 @@
+## Imports
+from pyspark import SparkConf, SparkContext
+
+## Module Constants
+APP_NAME = "Prime"
+
+def isprime(n):
+
+    """
+    check if integer n is a prime
+    """
+
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2:
+        return True
+    # all other even numbers are not primes
+    if not n & 1:
+        return False
+    # range starts with 3 and only needs to go up the square root of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
+#Main function
+def main(sc):
+
+    #Import data file
+    with open("/root/Distributed-Data-Mining-17/task1/spark/data/input_numbers.txt") as f:
+        for line in f:
+            numbers_raw = map(float, line.split())
+
+    #Split data
+    #numbers_split = numbers_raw.map(split)
+
+    #Broadcast data
+    #numbers_lookup = sc.broadcast(numbers_split)
+
+    # Create an RDD of numbers from 0 to 1,000,000
+    nums = sc.parallelize(numbers_raw)
+
+    # Compute the number of primes in the RDD
+    #primes_l = nums.filter(isprime).count()
+
+    # Sum up 
+    #primes_g  = primes_l.reduceByKey(add).collect()
+
+    #print primes_g
+    #print primes_l
+    print nums.filter(isprime).count()
+
+if  __name__ == "__main__":
+    ## Configure Spark
+    conf = SparkConf().setAppName(APP_NAME)
+    #conf = conf.setMaster("local[*]")
+    sc   = SparkContext(conf=conf)
+
+    #Execute Main function
+    main(sc)
+
